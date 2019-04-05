c18024649d606f68db49415e7913ecf62f36f44d
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2019-04-05 05:53:49 +0100

    Create to drink or not to drink.js

---
+++ to drink or not to drink.js
@@ -0,0 +1,31 @@
+//to drink or not to drink?
+
+var firstName = "John";
+var age = 17;
+
+//ternary statement
+age < 20 ? console.log("Version 1 - " + firstName + " won't drink beer today")
+: console.log("Version 1 - " + firstName + " can drink beer!");
+
+//shorter version
+var drink = age >= 20 ? firstName + " can drink Beer" : firstName + " will drink Juice";
+console.log("Version 2 -", (drink));
+ 
+//if/else statement solution   
+
+if (age < 20) {
+  console.log("Version 3 - " + firstName + " should drink Juice, too young to drink beer");
+} else {
+  console.log("Version 3 - " +firstName + "is old enough to drink beer")
+};
+
+// Switch solution 
+switch (age){
+  case age < 20:
+    console.log("version 4 - " + firstName + " drinking juice");
+    break;
+  default: 
+    console.log("version 4 - " + firstName + " drinking Beer");
+}
+
+console.log("*** end of snippet ***"); 
