0bee5cf1e4791338566e21b80b4ea738c632dbae
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2019-04-09 23:56:07 +0100

    Update Restaurant_tips_calculator.js

--- Restaurant_tips_calculator.js
+++ Restaurant_tips_calculator.js
@@ -1,53 +1,19 @@
-/*
-var cafeBill = 48;
-var bistroBill = 124;
-var restaurantBill = 268;
-
-var tipsSpend = []
-var itemizedSpending = [];
-
-
-function calculateBudget()
-*/
-
-
-/*
-function calculateBudget() {
-var bills = [48, 124, 268];
-var tipsSpend = [];
-var itemizedSpending = [];
-  for (i<0, i = this.bills.length, i++) {
-    if (i < 50) {
-    this.bills.[i] = i * 2 /100;
-    console.log([i]);
-  } else if (i >= 50 || i < 200){
-    this.bills.[i] = i * 1.5 /100;
-  } else if (i > 200) {
-    this.bills.[i] = i * 1 /100;
+ 
+function tipCalculator(bill) {
+  var percentage;
+  if(bill < 50) {
+    percentage = .2;
+  } else if(bill >=50 && bill < 200) {
+    percentage = .15;
+  } else {
+    percentage = .1;
   }
-};
-*/
-
-var bill
-
-
-function calculateBudget(bill) {
-
-  switch(true) {
-    case bill < 50:
-      tip = bill * 0.2;
-      console.log("The tip amount for this bill is equal to " + tip);
-      break;
-    case bill >=50 || bill < 200:
-      tip = bill * 0.15;
-      console.log("The tip amount for this bill is equal to " + tip);
-      break;
-    case bill >=200:
-      tip = bill * 0.1;
-      console.log("The tip amount for this bill is equal to " + tip);
-      break;
-  }
+  return percentage * bill;
 }
+console.log(tipCalculator());
 
-calculateBudget(300);
-
+var bills = [48, 124, 268]
+var tips = [tipCalculator(bills[0]),
+            tipCalculator(bills[1]),
+            tipCalculator(bills[2])];
+          console.log(tips);
