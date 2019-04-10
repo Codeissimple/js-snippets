2f0333a0b24f2496ef5e79a11497c13985a4a13a
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2019-04-10 23:09:56 +0100

    Solution added

--- BMI-calculator.js
+++ BMI-calculator.js
@@ -32,3 +32,35 @@ if (bmiMark > bmiJohn) {
 } else {
   console.log("Mark's BMI is lower, than John's");
 }
+
+// Object/Method solution
+
+
+
+var john = {
+  firstname: "John",
+  weight: 68,
+  height: 1.83,
+  calcBmi: function(){
+    this.bmi = this.weight / (this.height ^ 2);
+    return this.bmi;
+  }
+}
+ 
+var mark = {
+  firstName: "Mark",
+  weight: 78,
+  height: 1.69,
+  calcBmi: function(){
+    this.bmi = this.weight / (this.height ^ 2);
+    return this.bmi;
+  }
+}
+
+if (john.calcBmi() > mark.calcBmi()) {
+  console.log("John's BMI is higher, than Mark's at " + john.bmi);
+} else if(john.bmi < mark.bmi) {
+    console.log("Mark's BMI is higher, than John's at " + mark.bmi);
+} else if (john.bmi === mark.bmi){
+    console.log("John's and Mark's BMI are equal at  " + john.bmi);
+}
