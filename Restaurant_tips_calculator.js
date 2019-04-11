47b6f1c80ef55ff7c0d8166ab249f8e6aefa5a9e
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2019-04-11 05:44:43 +0100

    Solution added

--- Restaurant_tips_calculator.js
+++ Restaurant_tips_calculator.js
@@ -21,3 +21,37 @@ var final = [bills[0] + tips[0],
             bills[1] + tips[1],
             bills[2] + tips[2]];
 console.log(tips, final);
+
+
+// Solution 2
+
+
+var john = {
+  fullName: "John Smith",
+  bills: [124, 48, 268, 180, 42],
+  calcTips: function() {
+    this.tips = [];
+    this.totals = [];
+    
+    for (var i = 0; i < this.bills.length; i++){
+      //determing tipping percentage based on the tipping rulles
+      var percentage;
+      var bill = this.bills[i];
+       
+      if (bill = 50) {
+        percentage = 0.2;
+      } else if (bill >= 50 && bill < 200) {
+          percentage = 0.15;
+      } else {
+          percentage = 0.1;
+      }
+     }
+      
+      //populate the corresponding arrays
+      this.tips[i] = bill + percentage;
+      this.totals[i] = bill + (bill * percentage);
+    }
+  }
+
+john.calcTips();
+console.log(john);
